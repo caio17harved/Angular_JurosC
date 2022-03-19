0130d7471c6257c9f02704555a48d4cc63308e07
caio17harved <caio.croccia@gmail.com>
2022-03-18 22:45:46 -0300

    1 file modified, 1 added

--- src/app/jurosc/jurosc.component.ts
+++ src/app/jurosc/jurosc.component.ts
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./jurosc.component.css']
 })
 export class JuroscComponent implements OnInit {
-
-  constructor() { }
+  vp: number;
+  j: number;
+  n: number;
+  constructor() { 
+    this.vp = 100;
+    this.j = 2.5;
+    this.n = 3;
+  }
 
   ngOnInit() {
   }
+  getJuros(){
+  
+  for(var i = 0; i => this.n; i++ ) {
+    return this.vp * (1 + this.j/100) ** this.n
+    }
+  }
+
 
 }
